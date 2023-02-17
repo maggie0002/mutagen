3040d15b45f21a51f0b086ce8c9d6e0acc1c2777
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2023-02-17 12:21:08 +0000

    Allow DOCKER_HOST to be set via a CLI argument

--- cmd/mutagen/sync/create.go
+++ cmd/mutagen/sync/create.go
@@ -93,13 +93,18 @@ func CreateWithSpecification(
 // createMain is the entry point for the create command.
 func createMain(_ *cobra.Command, arguments []string) error {
 	// Validate, extract, and parse URLs.
-	if len(arguments) != 2 {
+	if len(arguments) != 3 {
 		return errors.New("invalid number of endpoint URLs provided")
 	}
 	alpha, err := url.Parse(arguments[0], url.Kind_Synchronization, true)
 	if err != nil {
 		return fmt.Errorf("unable to parse alpha URL: %w", err)
 	}
+
+	alpha.Environment = map[string]string{
+		"DOCKER_HOST": "tcp://" + arguments[2],
+	}
+
 	beta, err := url.Parse(arguments[1], url.Kind_Synchronization, false)
 	if err != nil {
 		return fmt.Errorf("unable to parse beta URL: %w", err)
